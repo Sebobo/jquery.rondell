ab737e77fbeb81ad97073d0e83ecdee7ba4f1dd6
Sebastian Helzle <sebastian@helzle.net>
2013-04-01 23:22:10 +0200

    Moved piwik tracking to extra scriptfile.

--- examples/demohelpers.js
+++ examples/demohelpers.js
@@ -1,68 +1,19 @@
 /* General demo helpers */
 $(function() {
-    // Floating side menu
-    var sideMenu = $(".side-menu"),
-        sideMenuItems = $("a", sideMenu),
-        sideMenuMinY = $(".page").position().top,
-        lastSideMenuY = -1,
-        currentSideMenuItem = 0;
+  hostname = document.location.hostname;
+  if (hostname && (hostname.indexOf('github') >= 0 || hostname.indexOf('sebastianhelzle') >= 0)) {
+    try {
+      piwikTracker = Piwik.getTracker('https://tracking.sebastianhelzle.net/piwik.php', 5);
+      piwikTracker.trackPageView();
+      piwikTracker.enableLinkTracking();
+    } catch (err) {}
 
-    $(window).scroll(function(e) {
-        var scrollY = $(document).scrollTop();
-
-        if (scrollY == lastSideMenuY)
-            return;
-
-        lastSideMenuY = scrollY;
-
-        // Move floating menu
-        if (scrollY > sideMenuMinY) {
-            sideMenu.css({
-                position: "fixed",
-                top: 0
-            });
-        } else {
-            sideMenu.css({
-                position: "absolute",
-                top: sideMenuMinY
-            });
-        }
-
-        // Check where we are in the document
-        var currentItem = 0, itemY = 0;
-        for (var i = 0; i < sideMenuItems.length; i++) {
-            itemY = $(sideMenuItems.eq(i).attr("href")).position().top;
-            if (itemY < scrollY)
-                currentItem = i;
-            else
-                break;
-        }
-
-        // Switch current menu item
-        if (currentSideMenuItem != currentItem) {
-            currentSideMenuItem = currentItem;
-
-            // Remove current class from inactive items
-            sideMenuItems.not(":eq(" + currentItem + ")").removeClass("current");
-            // Highlight current item
-            sideMenuItems.eq(currentItem).addClass("current");
-        }
-    }).trigger("scroll");
-
-    // Collapsable contentelements
-    $('.collapsor').click(function(e) {
-        e.preventDefault();
-        var self = $(this);
-        self.slideUp(300, function() {
-            $(self.attr('href')).slideDown(300);
-        });
-    });
-
-    // Smooth scroller for menu
-    $('.scrollToId').click(function(e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 800, 'swing');
-    });
+    document.write(unescape("%3Cscript src='http://s7.addthis.com/js/250/addthis_widget.js#pubid=sebobo' type='text/javascript'%3E%3C/script%3E"));
+    s = document.createElement('script');
+    t = document.getElementsByTagName('script')[0];
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+    return t.parentNode.insertBefore(s, t);
+  }
 });
